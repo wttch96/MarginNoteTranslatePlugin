7d2ed306137672d09fecc598ba8f62b55acc63d8
wttch <wttch@wttch.com>
2024-05-28 15:49:53 +0800

    Initial Commit

---
+++ MarginNoteTranslatePlugin/MarginNoteTranslatePluginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarginNoteTranslatePluginApp.swift
+//  MarginNoteTranslatePlugin
+//
+//  Created by Wttch on 2024/5/28.
+//
+
+import SwiftUI
+
+@main
+struct MarginNoteTranslatePluginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
