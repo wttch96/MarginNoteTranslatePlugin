70c3ef8bd8199ba22ab250a0806add2dade9c500
wttch <wttch@wttch.com>
2024-05-28 16:37:11 +0800

    init window

--- MarginNoteTranslatePlugin/ContentView.swift
+++ MarginNoteTranslatePlugin/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var receviedUrl: String? = nil
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(receviedUrl)")
         }
         .padding()
+        .onOpenURL(perform: { url in
+            receviedUrl = url.absoluteString.removingPercentEncoding
+        })
     }
 }
 
--- MarginNoteTranslatePlugin/MarginNoteTranslatePluginApp.swift
+++ MarginNoteTranslatePlugin/MarginNoteTranslatePluginApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct MarginNoteTranslatePluginApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-        WindowGroup {
+        Window("MarginNote插件", id: "WttchMarginNotePlugin", content: {
             ContentView()
+            
+        })
+    }
+}
+
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let window = NSApplication.shared.windows.first {
+            window.level = .floating
+            window.isMovableByWindowBackground = true
+            window.styleMask = [.borderless]
         }
     }
 }
